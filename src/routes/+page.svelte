992705f77e086fecdf04f9585c7600579796d8c2
Charlie Coppinger <charles.coppinger@gmail.com>
2024-05-03 14:09:12 +1200

    get in twitch player

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let twitchReady: boolean = false;
+
+	onMount(() => {
+		twitchReady = true;
+	});
+</script>
+
+<div class="h-3 w-full bg-pink-500"></div>
+<div class="h-3 w-full bg-violet-700"></div>
+<div class="h-3 w-full bg-blue-900"></div>
+
+<div class="min-w-screen flex min-h-screen flex-col items-center bg-neutral-950 p-8">
+	{#if twitchReady}
+		<iframe
+			loading="lazy"
+			title="thecoppinger Twitch Stream"
+			src="https://player.twitch.tv/?autoplay=true&muted=true&parent=www.thecoppinger.com&channel=thecoppinger"
+			height="300px"
+			width="400px"
+		>
+		</iframe>
+	{:else}
+		<div class="h-[300px] w-full bg-slate-500"></div>
+	{/if}
+</div>
