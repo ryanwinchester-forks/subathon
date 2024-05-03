26a9443935ca9b4a63da8547f655154bc1e306a8
Charlie Coppinger <charles.coppinger@gmail.com>
2024-05-03 13:35:39 +1200

    get in overlay timers and ui

---
+++ src/routes/auth/+page.server.ts
@@ -0,0 +1,35 @@
+import { redirect } from '@sveltejs/kit';
+
+import type { Actions } from './$types';
+
+export const actions: Actions = {
+	signup: async ({ request, locals: { supabase } }) => {
+		const formData = await request.formData();
+		const email = formData.get('email') as string;
+		const password = formData.get('password') as string;
+
+		const { error } = await supabase.auth.signUp({ email, password });
+		if (error) {
+			console.error(error);
+			return redirect(303, '/auth/error');
+		} else {
+			return redirect(303, '/');
+		}
+	},
+	login: async ({ request, locals: { supabase } }) => {
+		const { data, error } = await supabase.auth.signInWithOAuth({
+			provider: 'twitch',
+			options: {
+				redirectTo: 'localhost:5173/auth/confirm',
+				scopes: 'channel:read:subscriptions'
+			}
+		});
+
+		if (error) {
+			console.error(error);
+			return redirect(303, '/auth/error');
+		} else {
+			return redirect(303, data.url);
+		}
+	}
+};
---
+++ src/routes/overlay/+page.server.ts
@@ -0,0 +1,8 @@
+export const load = async ({ locals: { supabase } }) => {
+	const { data, error } = await supabase.from('end_time').select('*').single();
+	if (error) {
+		console.error(error);
+		return { error };
+	}
+	return { end_time: data };
+};
