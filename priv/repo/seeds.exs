bcbfe8bfd051bde5e2a06712dae50502608b7ab5
Ryan Winchester <ryan@winchester.dev>
2024-05-07 08:19:12 -0300

    fix spacing

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,7 +14,9 @@ alias Subathon.Accounts
 alias Subathon.Repo
 
 Repo.transaction(fn ->
-  profile =
+  Repo.insert!(%Subathon.EndTime{end_time: ~U[2024-08-02 00:01:55.000000Z]})
+
+  profile_a =
     Repo.insert!(%Accounts.Profile{
       twitch_id: "93766092",
       twitch_username: "TheCoppinger",
@@ -22,11 +24,23 @@ Repo.transaction(fn ->
         "https://static-cdn.jtvnw.net/jtv_user_pictures/b638380e-eef0-434d-a7e2-b6c68d7cb256-profile_image-70x70.png"
     })
 
-  Repo.insert!(%Subathon.EndTime{end_time: ~U[2024-08-02 00:01:55.000000Z]})
+  profile_b =
+    Repo.insert!(%Accounts.Profile{
+      twitch_id: "146616692",
+      twitch_username: "RyanWinchester_TV",
+      pfp_url:
+        "https://static-cdn.jtvnw.net/jtv_user_pictures/6d112a99-ae43-4111-90ed-2da6cb7ed622-profile_image-300x300.png"
+    })
 
   for date <- Date.range(~D[2024-05-03], ~D[2024-05-07]) do
     Repo.insert!(%Accounts.CheckIn{
-      profile_id: profile.id,
+      profile_id: profile_a.id,
+      date_nz: date,
+      inserted_at: DateTime.new!(date, ~T[12:00:00.000000])
+    })
+
+    Repo.insert!(%Accounts.CheckIn{
+      profile_id: profile_b.id,
       date_nz: date,
       inserted_at: DateTime.new!(date, ~T[12:00:00.000000])
     })
